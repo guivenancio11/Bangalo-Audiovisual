de96fca1a97138b89de9f6ac23660de4d5e3a5f3
Guilherme Venancio <69999751+guivenancio11@users.noreply.github.com>
2021-02-25 09:15:12 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,47 @@
+		<!-- Messenger Plugin -->
+		<?php include('messenger-plugin.php'); ?>
+		<!-- END Messenger Plugin -->
+		
+		<footer class="bg-dark p-4">
+			<div class="pt-2">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<center>
+								<img src="<?php bloginfo('template_directory'); ?>/img/logo-branca.svg" width="110" height="95">
+							</center>
+						</div>
+						<div class="col-md-4">
+							<p class="text-light text-center p-2" style="font-size:13px;">
+								<b>BANGALÔ AUDIOVISUAL</b><br>
+								CNPJ: 35.335.106/0001-20<br>
+								Balneário Piçarras - SC<br>
+								© 2019-2020 Bangalô Audiovisual
+							</p>
+						</div>
+						<div class="col-md-4 text-center p-4">
+					        <a href="https://facebook.com/bangaloaudiovisual" target="blank"><span class="text-light"><i class="fab fa-2x fa-facebook-f">&nbsp;&nbsp;</i></span></a>
+					        <a href="https://instagram.com/bangaloaudiovisual" target="blank"><span class="text-light"><i class="fab fa-2x fa-instagram">&nbsp;&nbsp;</i></span></a>
+					        <a href="https://www.youtube.com/channel/UCuetd4HMrlA3JbVJTDpSbew" target="blank"><span class="text-light"><i class="fab fa-2x fa-youtube">&nbsp;&nbsp;</i></span></a>
+					        <a href="https://bit.ly/34rT9sf" target="blank"><span class="text-light"><i class="fab fa-2x fa-whatsapp">&nbsp;&nbsp;</i></span></a>
+					        <a href="https://bit.ly/31OAjrQ" target="blank"><span class="text-light"><i class="fab fa-2x fa-telegram"></i></span></a>
+						</div>
+					</div>
+			    </div>
+			</div>
+		</footer>
+
+		<!-- jQuery Boostrap -->
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+		<!-- END jQuery Bootstrap -->
+
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+		<!-- END Google Fonts -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>	
